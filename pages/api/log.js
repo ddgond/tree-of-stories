662233cb5291707228bba32428c409cf9fb2a3f7
Daniel Dangond <dadangond@protonmail.com>
2023-02-08 14:11:26 -0500

    Publish statistics

--- pages/api/log.js
+++ pages/api/log.js
@@ -2,5 +2,5 @@ import Statistics from "../../lib/Statistics";
 
 export default function catchAll(req, res) {
   Statistics.log();
-  res.status(404).json({message: "This API does not exist"});
+  res.status(200).send(Statistics.toString());
 }
